f0edabd527f16124fabbe6e8f115cbcc4e571724
tolikrss <tolikrss@gmail.com>
2017-04-10 13:35:35 +0300

    add new version

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
-var fl = false;
+var fl = false,
+    activeSlide = 0,
+    sliders = document.getElementsByClassName('slide');
+console.log(sliders);
 
 function toggle() {
     var menu = document.getElementById('menu-show');
@@ -9,4 +12,35 @@ function toggle() {
         menu.style.display = 'block';
         fl = !fl;
     }
+}
+
+function displayNone() {
+    for (var i = 0; i < sliders.length; i++) {
+        if (i !== activeSlide) {
+            sliders[i].style.display = 'none';
+        }
+    }
+}
+
+function next() {
+    // Array.fotEach.call(sliders, function(element) {
+    //
+    // });
+    if (activeSlide === (sliders.length - 1)) {
+        activeSlide = 0;
+    } else {
+        activeSlide++;
+    }
+    sliders[activeSlide].style.display = 'block';
+    displayNone();
+}
+
+function previous() {
+    if (activeSlide === 0) {
+        activeSlide = sliders.length - 1;
+    } else {
+        activeSlide--;
+    }
+    sliders[activeSlide].style.display = 'block';
+    displayNone();
 }
