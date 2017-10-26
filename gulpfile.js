69e3efc6c112f07f702e233b812ffe876981941d
a.lahno <tolikrss@gmail.com>
2017-10-26 17:10:25 +0300

    init test

--- gulpfile.js
+++ gulpfile.js
@@ -15,41 +15,44 @@ var ngmin        = require('gulp-ngmin');
       }))
       .pipe(gulp.dest('css'));
   });
+  
 
   gulp.task('html', function(){
     gulp.src('*.html');
-  });  
+  });
+  
   
   gulp.task('scripts', function(){
     return gulp.src(paths.script)
       .pipe(gulp.dest('./js/'));
   });
 
-  gulp.task('css-min', function () {
-    return gulp.src('scss/**/*.scss')
-        .pipe(concat('style.min.css'))
-        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-        .pipe(autoprefixer({
-            browsers: ['last 16 versions'],
-            cascade: false
-        }))
-        .pipe(gulp.dest('css'));
-  });
+  // gulp.task('css-min', function () {
+  //   return gulp.src('scss/**/*.scss')
+  //       .pipe(concat('style.min.css'))
+  //       .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+  //       .pipe(autoprefixer({
+  //           browsers: ['last 16 versions'],
+  //           cascade: false
+  //       }))
+  //       .pipe(gulp.dest('css'));
+  // });
 
-  gulp.task('js-min', function () {
-    return gulp.src('js/main.js')
-        .pipe(concat('main.min.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('js'));
-  });
+  // gulp.task('js-min', function () {
+  //   return gulp.src('js/main.js') //return gulp.src('app/js/**/*.js')
+  //       .pipe(concat('main.min.js')) //.pipe(concat('build.min.js'))
+  //       .pipe(uglify())
+  //       .pipe(gulp.dest('js'));
+  // });
+  
   
   gulp.task('watch', ['sass'], function (){
     gulp.watch('scss/**/*.scss', ['sass']); 
 
-    gulp.watch('scss/**/*.scss', ['css-min']); 
-    gulp.watch('js/main.js', ['js-min']); 
-  });
+    // gulp.watch('scss/**/*.scss', ['css-min']); 
+    // gulp.watch('js/main.js', ['js-min']); 
+  })
 
-gulp.task('build', ['css-min', 'js-min']);
+// gulp.task('build', ['css-min', 'js-min']);
 
 gulp.task('default', ['watch']);
--- js/main.js
+++ js/main.js
@@ -1,28 +1,144 @@
-'use strict';
-(function() {
-    console.log('Scripts works');
+function Calc (str) {
+    var RegExGaps = /\s/g,
+        incStr    = '(' + str + ')',
+        Operands  = [],
+        Functions = [],
+        position  = 0,
+        token     = {},
+        prevToken = '';
+    
+    this.canPop = function (op1, Functions) {
+        if (Functions.length == 0) return false;
 
-    function getRotationDegrees(obj) {
-        var matrix = obj.css("-webkit-transform") ||
-        obj.css("-moz-transform")                 ||
-        obj.css("-ms-transform")                  ||
-        obj.css("-o-transform")                   ||
-        obj.css("transform");
-        if(matrix !== 'none') {
-            var values = matrix.split('(')[1].split(')')[0].split(',');
-            var a = values[0];
-            var b = values[1];
-            var angle = Math.round(Math.atan2(b, a) * (180/Math.PI));
-        } else { var angle = 0; }
-        return (angle < 0) ? angle + 360 : angle;
+        var p1 = getPriority( op1 ),
+            p2 = getPriority( Functions[Functions.length - 1] );
+    
+        return p1 >= 0 && p2 >= 0 && p1 >= p2;
+    };
+    
+    this.getPriority = function (operand) {
+        switch (operand) {
+            case '(': return -1;
+            case '*': return 1;
+            case '/': return 1;
+            case '+': return 2;
+            case '-': return 2;
+        };
+    };
+    
+    this.IsDigit = function (s) {
+        return (!isNaN(s) && s.length === 1) ? true : false;
     };
 
-    $('document').ready(function(){
+    incStr = incStr.replace(RegExGaps, '');
+
+    while (token != null) {
+
+        if (position === incStr.length) {
+            token = null;
+        };
+
+        if(IsDigit(incStr[position])) {
+            var res = "";
+
+            while (position < incStr.length && (IsDigit(incStr[position]) || incStr[position] == '.')) {
+                res += incStr[position++];
+            }
+
+            token = Number(res);
+        } else {
+            token = incStr[position++];
+        };
+
+        if ( typeof token === 'string'     && 
+             typeof prevToken === 'string' &&
+             prevToken.toString() == '('   &&
+             (token.toString() == '+' || token.toString() == '-')
+           ) 
+        {
+            Operands.push(0);
+        }
+
+        if (typeof token === 'number') {            
+            Operands.push( Number(token) );
+        } else if (typeof token === 'string') { 
+            if (token.toString() == ')') {
+                while (Functions.length > 0 && Functions[Functions.length - 1] != '(') {
+                    var B = Operands.pop(),
+                        A = Operands.pop();
+
+                    switch (Functions.pop())
+                    {
+                        case '+': Operands.push(A + B);
+                                  break;
+                        case '-': Operands.push(A - B);
+                                  break;
+                        case '*': Operands.push(A * B);
+                                  break;
+                        case '/': Operands.push(A / B);
+                                  break;
+                    }
+                }
+                Functions.pop();
+            } else {
+                while (canPop(token.toString(), Functions)) {                    
+                    var B = Operands.pop(),
+                        A = Operands.pop();
+
+                    switch (Functions.pop()) {
+                        case '+': Operands.push(A + B);
+                                  break;
+                        case '-': Operands.push(A - B);
+                                  break;
+                        case '*': Operands.push(A * B);
+                                  break;
+                        case '/': Operands.push(A / B);
+                                  break;
+                    };
+                };
+                Functions.push(token.toString());
+            }
+        }
+        prevToken = token;
+    };
+
+    if (Operands.length > 1 || Functions.length > 0) {
+        console.error("Ошибка в разборе выражения");
+        return 'error';
+    };
+
+    return Operands.pop();
+};
+
+$( document ).ready(function() {
+    var screen      = $('.screen-container .screen'),
+        screenValue = '';
     
+    screen.html(0);
+    
+    $('button.btn').click(function() {
+        screenValue += $(this).html();
+        screen.html(screenValue);
     });
 
-    window.onload = function() {
-        console.log("window load occurred!");
-        $('.preloader-overlay').css('display', 'none');
-    };
-}());
+    $('button.result').click(function() {
+        var result = Calc(screenValue);
+        screen.html(result);
+        screenValue = '';
+    });
+
+    $('button.clear').click(function() {
+        screenValue = '';
+        screen.html(0);
+    });
+
+    $('button.power').click(function() {
+        if(screen.html().length > 0) {
+            screenValue = '';
+            screen.html('');
+        } else {
+            screenValue = '';
+            screen.html(0);
+        };
+    });
+});
